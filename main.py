a6c18fcfea2c7de1d28bd9e1b78fb1bcfa6c4d4f
Artemiy482 <149660349+Artemiy482@users.noreply.github.com>
2026-03-01 17:30:10 +0300

    square and circle setup

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pygame
-
+from random import randint
 pygame.init()
 WIDTH = 800
 LENGTH = 600
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 ORANGE = (255, 165, 0)
+x = randint(0, WIDTH - 1)
+y = randint(0, LENGTH - 1)
+x1 = randint(0, WIDTH - 1)
+y2 = randint(0, WIDTH - 1)
+SIZE = 50
 # создаем окно размера 800 на 600
 screen = pygame.display.set_mode((WIDTH,LENGTH))
 screen.fill(pygame.Color(WHITE))
@@ -21,6 +26,7 @@ while True:
         if e.type == pygame.QUIT:
             # закрыть окно
             raise SystemExit("QUIT")
-
+    pygame.draw.circle(screen, (255, 0 , 0), (x, y), 10)
+    pygame.draw.rect(screen, (255,165,0), (x1, y2, 50, 50))
     # перерисовать окно
     pygame.display.update()
