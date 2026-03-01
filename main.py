b201e5f9b837745146c0808dbdc9eede7bf57223
Artemiy482 <149660349+Artemiy482@users.noreply.github.com>
2026-03-01 17:00:46 +0300

    colour setup and display setup

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 import pygame
+
+pygame.init()
+WIDTH = 800
+LENGTH = 600
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+ORANGE = (255, 165, 0)
+# создаем окно размера 800 на 600
+screen = pygame.display.set_mode((WIDTH,LENGTH))
+screen.fill(pygame.Color(WHITE))
+
+# указываем название
+pygame.display.set_caption("Snake")
+
+# игровой цикл
+while True:
+    # обрабатываем события
+    for e in pygame.event.get():
+        # если нажали на крестик
+        if e.type == pygame.QUIT:
+            # закрыть окно
+            raise SystemExit("QUIT")
+
+    # перерисовать окно
+    pygame.display.update()
