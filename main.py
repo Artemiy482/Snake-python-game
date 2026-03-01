6045e1de137d9f1f7bb17486279947d330de52ab
Artemiy482 <149660349+Artemiy482@users.noreply.github.com>
2026-03-01 17:39:50 +0300

    other items add

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ ORANGE = (255, 165, 0)
 x = randint(0, WIDTH - 1)
 y = randint(0, LENGTH - 1)
 x1 = randint(0, WIDTH - 1)
+y1 = randint(0, WIDTH - 1)
+x2 = randint(0, WIDTH - 1)
 y2 = randint(0, WIDTH - 1)
+x3 = randint(0, WIDTH - 1)
+y3 = randint(0, WIDTH - 1)
 SIZE = 50
 # создаем окно размера 800 на 600
 screen = pygame.display.set_mode((WIDTH,LENGTH))
@@ -27,6 +31,8 @@ while True:
             # закрыть окно
             raise SystemExit("QUIT")
     pygame.draw.circle(screen, (255, 0 , 0), (x, y), 10)
-    pygame.draw.rect(screen, (255,165,0), (x1, y2, 50, 50))
+    pygame.draw.rect(screen, (255,165,0), (x1, y2, 50,50))
+    pygame.draw.circle(screen, (0, 0, 255), (x2, y2), 10)
+    pygame.draw.circle(screen, (139,0,255), (x3, y3), 10)
     # перерисовать окно
     pygame.display.update()
